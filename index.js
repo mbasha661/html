7febf1535ed3cae47fb3ac99aea980e9fc05fd12
mbasha661 <mbasha661@gmail.com>
2023-12-11 04:51:13 -0800

    js

---
+++ index.js
@@ -0,0 +1,29 @@
+console.log("basha");
+console.log("BALA");
+console.log("mani");
+//variables
+let bala=80
+var basha=800
+const mani=true
+
+console.log(bala)
+console.log(basha)
+console.log(mani)
+//premitive data types
+let a='hello'
+let b=55
+let c
+let d=null
+let e = true
+console.log(typeof a,typeof b,typeof c,typeof d,typeof e)
+//Object
+let student={
+    name: 'basha',
+    course:'full statck',
+    dateofjoin:'11-12-2023',
+
+}
+console.log(student.name)
+console.log(student)
+console.log(student.dateofjoin)
+console.log(student)
